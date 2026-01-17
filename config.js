8e35e96a6903864501b5e08bac3c5386c7670c04
Reza Rizki <rezarizkyramadhan1233@gmail.com>
2026-01-17 10:38:03 +0700

    update local tunnel

---
+++ config.js
@@ -0,0 +1,8 @@
+// Cukup ganti di sini saja bro pas presentasi
+const CONFIG = {
+    // URL dari Localtunnel yang lo dapet tadi
+    API_BASE_URL: "https://weak-ends-shop.loca.lt/api"
+};
+
+// Biar bisa dipake di file lain
+window.CONFIG = CONFIG;
